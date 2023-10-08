667f09dd669c8026091a3e5f9c682c0434a7d3b0
Fahad Alamgir Dhruba <dhruba.fahad2004@gmail.com>
2023-10-08 23:47:58 +0600

    Add your_file_name

---
+++ bookmarklet.js
@@ -0,0 +1,28 @@
+javascript: (function () {
+    function copyToClipboard(text) {
+        const textArea = document.createElement("textarea");
+        textArea.value = text;
+        document.body.appendChild(textArea);
+        textArea.select();
+        document.execCommand("copy");
+        document.body.removeChild(textArea);
+      }
+      
+      var iframe = document.querySelector("iframe[allow]");
+      var gcloud = document.querySelector("video[src]");
+      
+      if (gcloud) {
+        var srclink = gcloud.src;
+        copyToClipboard(srclink);
+        alert(
+          "Google Cloud Link Copied...!!! \n \n Paste It Into Suggested Video Player..."
+        );
+      } else if (iframe) {
+        var src = iframe.src;
+        var youtoob = "https://www.youtube.com/watch?v=" + src.slice(30, 41);
+        copyToClipboard(youtoob);
+        alert("Youtoob Link Copied...!!! \n ");
+      } else {
+        alert("Boom");
+      }
+})();
